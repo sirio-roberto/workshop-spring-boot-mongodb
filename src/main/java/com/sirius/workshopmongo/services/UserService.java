0db162586de198093d2cf850405631a6b0afb27a
Sirio Junior <sirioroberto1@hotmail.com>
2022-12-02 06:56:42 -0300

    Obtendo um usuario pelo id

--- src/main/java/com/sirius/workshopmongo/services/UserService.java
+++ src/main/java/com/sirius/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.sirius.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sirius.workshopmongo.domain.User;
 import com.sirius.workshopmongo.repository.UserRepository;
+import com.sirius.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,13 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		User user = repo.findById(id).orElse(null);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
 	}
 }
